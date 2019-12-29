0582cd92a6320a5ef348e4ca396b13896709b3ce
Daniel Thwaites <danthwaites30@btinternet.com>
2019-12-29 17:08:35 +0000

    Display completed training in embed

--- chatterbot/train.py
+++ chatterbot/train.py
@@ -1,5 +1,6 @@
 import logging
 
+import discord
 from discord.ext import commands
 from chatterbot.trainers import ListTrainer
 
@@ -33,7 +34,39 @@ class Training(commands.Cog):
 
         # Completed, respond to command
         logger.info('Done!')
-        await ctx.send('Done!')
+        await self.show_training(statements, ctx)
+
+    async def show_training(self, statements, ctx):
+        """Send a message showing the completed training."""
+
+        # Build a string showing the conversation
+        conversation = str()
+        person_toggle = False
+        for statement in statements:
+            # Toggle between person A and person B
+            person_toggle = not person_toggle
+            if person_toggle: person = 'A'
+            else:             person = 'B'
+            # Add a line for this statement
+            conversation += f'{person}: {statement}\n'
+
+        # Send in embed as response to command
+        e = discord.Embed(
+            title='Training Completed',
+            description=f'```\n{conversation}```',
+        )
+        e.set_author(
+            name=ctx.author.name,
+            icon_url=ctx.author.avatar_url
+        )
+        await ctx.send(embed=e)
+
+        try:
+            # Delete the command message
+            await ctx.message.delete()
+        except discord.Forbidden:
+            # It doesn't matter if we can't
+            pass
 
 
 def setup(bot):
