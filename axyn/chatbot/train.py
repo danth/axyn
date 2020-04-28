8d5236905abcaf81d04d514c649dd6a520b29544
Daniel Thwaites <danthwaites30@btinternet.com>
2020-04-28 19:45:55 +0100

    feat(chatbot): handle multiple responses to one input
    BREAKING CHANGE: Separate the `id` and `ngt_id` columns to allow for looking up multiple responses to the same vector.

--- axyn/chatbot/train.py
+++ axyn/chatbot/train.py
@@ -18,10 +18,16 @@ def train_statement(text, responding_to, session):
     :param session: Database session to use for insertions.
     """
     responding_to_vector = average_vector(responding_to)
-    # Add vector to NGT index
-    ngt_id = statements_index.insert(responding_to_vector)
-    statements_index.build_index()
-    statements_index.save()
+
+    result = statements_index.search(responding_to_vector, 1)
+    if len(result) > 0 and result[0][1] == 0:
+        # This vector is already in the index
+        ngt_id = result[0][0]
+    else:
+        # Add vector to the index
+        ngt_id = statements_index.insert(responding_to_vector)
+        statements_index.build_index()
+        statements_index.save()
 
     # Add response text to database
     session.add(Statement(ngt_id=ngt_id, text=text))
@@ -37,10 +43,16 @@ def train_reaction(emoji, responding_to, session):
     :param session: Database session to use for insertions.
     """
     responding_to_vector = average_vector(responding_to)
-    # Add vector to NGT index
-    ngt_id = reactions_index.insert(responding_to_vector)
-    reactions_index.build_index()
-    reactions_index.save()
+
+    result = reactions_index.search(responding_to_vector, 1)
+    if len(result) > 0 and result[0][1] == 0:
+        # This vector is already in the index
+        ngt_id = result[0][0]
+    else:
+        # Add vector to the index
+        ngt_id = reactions_index.insert(responding_to_vector)
+        reactions_index.build_index()
+        reactions_index.save()
 
     # Add reaction to database
     session.add(Reaction(ngt_id=ngt_id, emoji=emoji))
